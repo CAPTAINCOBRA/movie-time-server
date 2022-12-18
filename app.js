fa1f40b5f7bdb29f894156b14282d5f921559b76
Ekansh Baweja <ekansh.baweja@gmail.com>
2022-12-18 23:57:55 +0530

    One

--- app.js
+++ app.js
@@ -39,9 +39,12 @@ app.use((req, res, next) => {
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://movie-time-theta.vercel.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
